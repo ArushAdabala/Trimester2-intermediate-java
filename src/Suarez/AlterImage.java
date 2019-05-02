47a27ff9b8d1e2857fea02f48bbc2887d470acb7
ArushAdabala <47830364+ArushAdabala@users.noreply.github.com>
2019-05-02 18:37:25 -0500

    Update AlterImage.java

--- src/Suarez/AlterImage.java
+++ src/Suarez/AlterImage.java
@@ -3,27 +3,23 @@ package Suarez;
 import java.util.ArrayList;
 
 public class AlterImage {
-    private ArrayList<Pixel> pixelArray = new ArrayList<Pixel>();
     public Pixel[][] generatePixelArray(int [][] reds,int [][] greens,int [][] blues){
+        Pixel[][] pixelArray = new Pixel[reds.length][reds[reds.length-1].length];
         for(int i = 0; i < reds.length; i++){
             for(int j = 0; j < reds[i].length; j++) {
                 Pixel pixel = new Pixel(reds[i][j],greens[i][j],blues[i][j]);
-                pixelArray.add(pixel);
+                pixelArray[i][j] = pixel;
             }
         }
-        Pixel[][] truePixelArray = new Pixel[reds.length][(pixelArray.size())/(reds.length)];
-        for(int i = 0; i < truePixelArray.length; i++){
-            for(int j = 0; j < truePixelArray[i].length; j++) {
-                truePixelArray[i][j] = pixelArray.get(i*j);
-            }
-        }
-        return truePixelArray;
+        
+
+        return pixelArray;
     }
     public Pixel[][] flipImage(Pixel[][] image, boolean horiz){
-        Pixel [] [] temp;
+        Pixel [] [] temp = new Pixel[image.length][image[image.length-1].length];;
         for(int i = 0; i < image.length; i++){
             for(int j = 0; j < image[i].length; j++) {
-                temp = new Pixel[image.length][image[i].length];
+                
                 temp[i][j] = image[i][j];
                 image[i][j] = image[image.length-i][image[i].length-j];
                 image[image.length-i][image[i].length-j]=image[i][j];
@@ -33,3 +29,4 @@ public class AlterImage {
     }
 
 }
+
